3bf0d7b1ec8a435a5e75d9b3477f86318da003f8
Benny-Chung <78866329+Benny-Chung@users.noreply.github.com>
2023-09-22 01:01:12 +0800

    Add files via upload

---
+++ task3.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.sql.Row
+
+import org.apache.spark.rdd.RDD
+
+val df = spark.read.format("csv").option("header",true).option("sep","\t").load("C:/Users/user/Downloads/Hadoop/search_data.csv")
+
+val lineRDD: RDD[String] = df.select("queryTime").rdd.map(_.mkString(""))
+
+val wordRDD: RDD[String] = lineRDD.map(line => line.dropRight(3))
+
+val kvRDD: RDD[(String, Int)] = wordRDD.map(word => (word, 1)) 
+
+val wordCounts: RDD[(String, Int)] = kvRDD.reduceByKey((x, y) => x + y)
+
+val exchangeRDD: RDD[(Int, String)] = wordCounts.map{case (k,v)=>(v,k)}
+
+val sortRDD: RDD[(Int, String)] = exchangeRDD.sortByKey(false)
+
+sortRDD.take(10).foreach(println)
